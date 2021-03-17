47e3802191ab20d89d85d2b9cd16bda43d829423
mikelovelace <mike@mlovelace.net>
2021-03-16 20:24:50 -0400

    added click event to articles

--- components/Article.js
+++ components/Article.js
@@ -4,7 +4,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -21,11 +21,11 @@ const data = [
     thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
         naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
         han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
-        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`,
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -41,11 +41,11 @@ const data = [
     thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights 
         Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven 
         roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot 
-        sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
+        sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`,
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -69,11 +69,11 @@ const data = [
 
     thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel 
         Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James 
-        Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
+        Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`,
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -85,8 +85,8 @@ const data = [
 
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
-          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  }
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`,
+  },
 ];
 
 /*
@@ -117,39 +117,44 @@ const data = [
 
 function articleMaker(article) {
   // create elements
-  const articleContainer = document.createElement("div") // main article container
-  const articleTitle = document.createElement("h2") // article title
-  const articleDate = document.createElement("p") // article date
-  const articlePara1 = document.createElement("p") // first paragraph
-  const articlePara2 = document.createElement("p") // second paragraph
-  const articlePara3 = document.createElement("p") // third paragraph
-  const articleButton = document.createElement("span") // expand button
+  const articleContainer = document.createElement("div"); // main article container
+  const articleTitle = document.createElement("h2"); // article title
+  const articleDate = document.createElement("p"); // article date
+  const articlePara1 = document.createElement("p"); // first paragraph
+  const articlePara2 = document.createElement("p"); // second paragraph
+  const articlePara3 = document.createElement("p"); // third paragraph
+  const articleButton = document.createElement("span"); // expand button
 
   // add classes
-  articleContainer.classList.add("article")
-  articleDate.classList.add("date")
-  articleButton.classList.add("expandButton")
+  articleContainer.classList.add("article");
+  articleDate.classList.add("date");
+  articleButton.classList.add("expandButton");
 
   // pull info from data object and add to elements
-  articleTitle.textContent = article.title
-  articleDate.textContent = article.date
-  articlePara1.textContent = article.firstParagraph
-  articlePara2.textContent = article.secondParagraph
-  articlePara3.textContent = article.thirdParagraph
-  articleButton.textContent = "Show More"
+  articleTitle.textContent = article.title;
+  articleDate.textContent = article.date;
+  articlePara1.textContent = article.firstParagraph;
+  articlePara2.textContent = article.secondParagraph;
+  articlePara3.textContent = article.thirdParagraph;
+  articleButton.textContent = "Show More";
 
   // append children to parent
-  articleContainer.append(articleTitle)
-  articleContainer.append(articleDate)
-  articleContainer.append(articlePara1)
-  articleContainer.append(articlePara2)
-  articleContainer.append(articlePara3)
-  articleContainer.append(articleButton)
+  articleContainer.append(articleTitle);
+  articleContainer.append(articleDate);
+  articleContainer.append(articlePara1);
+  articleContainer.append(articlePara2);
+  articleContainer.append(articlePara3);
+  articleContainer.append(articleButton);
 
-  return (articleContainer)
+  // add click event
+  articleButton.addEventListener("click", () => {
+    articleContainer.classList.toggle("article-open");
+  });
+
+  return articleContainer;
 }
 
-data.forEach(article => {
-  const selectArticle = document.querySelector(".articles")
-  selectArticle.append(articleMaker(article))
-})
+data.forEach((article) => {
+  const selectArticle = document.querySelector(".articles");
+  selectArticle.append(articleMaker(article));
+});
